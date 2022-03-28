5d47f9509e727481c732ad552a907bbd0f14a2b5
Michael Appiah-Kubi <kubiappiahmichael@gmail.com>
2022-03-28 17:27:58 +0000

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {View, Image, Text} from 'react-native';
+import {MaterialCommunityIcons} from '@expo/vector-icons'
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
+
+import AppText from './app/components/AppText';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      
+    <AppText>I love React Native!</AppText>
+    <MaterialCommunityIcons name='email' size={200} color= 'green'/>
+
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
