5fe0671edcaa99be38a1f0acc4eeed943563bb5d
Michael Appiah-Kubi <kubiappiahmichael@gmail.com>
2022-03-29 13:40:46 +0000

    Built the WelcomeScreen Component.

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { ImageBackground, StyleSheet, View, Image, Text } from 'react-native';
+import AppButton from "../components/AppButton";
 
 //const image = { uri: "https://reactjs.org/logo-og.png" };
 
 function WelcomeScreen(props) {
     return (
         <ImageBackground 
+        blurRadius={10}
             style={styles.background}
             source={require("../assets/background.jpg")}
         >
             <View style={styles.logoContainer}>
                 <Image style={styles.logo} source={require("../assets/logo-red.png")}/>
-                <Text >Sell What You Don't Need!</Text>
+                <Text style={styles.tagline}>Sell What You Don't Need!</Text>
             </View>
 
-            <View style={styles.loginButton}></View>
-            <View style={styles.registerButton}></View>
+            <View style={styles.buttonsContainer}>
+                <AppButton title="Login"></AppButton>
+                <AppButton title="Register" color='secondary'></AppButton>
+            </View>
         </ImageBackground> 
     );
 }
@@ -27,10 +31,15 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
 
-    loginButton: {
-        width: "100%",
-        height: 70,
-        backgroundColor: "#fc5c65",  
+    buttonsContainer: {
+        padding: 20,
+        width: '100%',
+    },
+
+    tagline: {
+        fontSize: 25,
+        fontWeight: '600',
+        paddingVertical: 20,
     },
 
     logo: {
@@ -41,15 +50,9 @@ const styles = StyleSheet.create({
     logoContainer: {
         position: 'absolute',
         top: 70,
-        alignItems: 'center',
-        
+        alignItems: 'center',   
     },
 
-    registerButton: {
-        width: '100%',
-        height: 70,
-        backgroundColor: "#4ecdc4",
-    }
 })
 
 export default WelcomeScreen;
