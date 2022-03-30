71aedabaa4444ae312e4e2dcfb9adf854295831f
Michael Appiah-Kubi <kubiappiahmichael@gmail.com>
2022-03-30 12:25:41 +0000

    Added ListItem 2 my ListingDetailsScreen Component

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -3,6 +3,8 @@ import {View, Image, StyleSheet} from 'react-native';
 import AppText from '../components/AppText';
 import colors from '../config/colors';
 
+import ListItem from '../components/ListItem';
+
 function ListingDetailsScreen(props) {
     return (
         <View>
@@ -11,6 +13,14 @@ function ListingDetailsScreen(props) {
             <View style={styles.detailsContainer}>
                 <AppText style={styles.title}>Red jacket for sale</AppText>
                 <AppText style={styles.price}>$100</AppText>
+
+                <View style={styles.userContainer}>
+                    <ListItem
+                        image={require("../assets/mosh.jpg")}
+                        title="Mosh Hamedani"
+                        subTitle="5 Listings"
+                    />
+                </View>
             </View> 
         </View>
     );
@@ -36,7 +46,11 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 24,
         fontWeight: '500',
-    }
+    },
+
+    userContainer: {
+        marginVertical: 40,
+    },
 })
 
 export default ListingDetailsScreen;
