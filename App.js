ba432774e97b0fba97fe34a820e8c07da6a2896f
Michael Appiah-Kubi <kubiappiahmichael@gmail.com>
2022-03-30 12:36:46 +0000

    Improved the ViewImage Screen

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {View, Image} from 'react-native';
 
 import Card from './app/components/Card';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 
 }
